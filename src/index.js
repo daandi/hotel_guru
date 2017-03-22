a9bca3bc985aa3881e3ad905b4c7ccc2af108576
Andreas Neumann <andreas.neumann@holidaycheck.com>
2017-03-22 13:57:41 +0100

    tr emit with if else

--- src/index.js
+++ src/index.js
@@ -38,42 +38,42 @@ const handlers = {
         const hotel = this.event.request.intent.slots.HOTEL.value;
         console.log('passion:' + passion + " " + "hotel:" + hotel);
 
-        if (! passion == null) {
+        if (passion) {
             this.attributes[PASSION_KEY] = passion;
         }
-        if (! hotel == null) {
+        if (hotel) {
             this.attributes[HOTEL_KEY] = hotel;
         }
 
         if(this.attributes[PASSION_KEY] == null && passion == null) {
             this.emit(':ask', "Zu welchem Thema möchtest du etwas wissen?", "Sage z.B. Zum Thema Essen.");
         }
-
-        if(this.attributes[HOTEL_KEY] == null && hotel == null) {
+        else if(this.attributes[HOTEL_KEY] == null && hotel == null) {
             this.emit(':ask', "Zu welchem Hotel möchtest du etwas wissen?", "Sage z.B. Hotel Dana Beach oder Das Adlon Berlin");
         }
+        else {
+            console.log('Got passion and hotel: passion:' + passion + " " + "hotel:" + hotel);
 
-        console.log('Got passion and hotel: passion:' + passion + " " + "hotel:" + hotel);
+            psc.getHotelUUID(hotel).then( (hotelObj) => {
+              psc.getHotelReviewsAsArray(hotelObj.id, passion).then( (answers) => {
+                const speechAnswer =
+                    `Holidaycheck Urlauber zum Thema ${passion} im ${hotelObj.name} <break time="1.5s"/>  ${psc.reviewsSpeechAnswer(answers)}`;
+                const imageObj = {
+                    smallImageUrl: `https://media-cdn.holidaycheck.com/w_310,h_280,c_fill,q_80/ugc/images/${hotelObj.id}`,
+                    largeImageUrl: `https://media-cdn.holidaycheck.com/w_1920,h_1080,c_fit,q_80/ugc/images/${hotelObj.id}`
+                };
+                const cardTextAnswer = psc.reviewsTextAnswer(answers);
 
-        psc.getHotelUUID(hotel).then( (hotelObj) => {
-          psc.getHotelReviewsAsArray(hotelObj.id, passion).then( (answers) => {
-            const speechAnswer =
-                `Holidaycheck Urlauber zum Thema ${passion} im ${hotelObj.name} <break time="1.5s"/>  ${psc.reviewsSpeechAnswer(answers)}`;
-            const imageObj = {
-                smallImageUrl: `https://media-cdn.holidaycheck.com/w_310,h_280,c_fill,q_80/ugc/images/${hotelObj.id}`,
-                largeImageUrl: `https://media-cdn.holidaycheck.com/w_1920,h_1080,c_fit,q_80/ugc/images/${hotelObj.id}`
-            };
-            const cardTextAnswer = psc.reviewsTextAnswer(answers);
-
-            this.emit(':tellWithCard', speechAnswer, hotelObj.name, cardTextAnswer, imageObj);
-        }).error((err) => {
-              this.attributes[PASSION_KEY] = null;
-              this.emit(':ask', `Zu Passion ${passion} weiß ich leider nichts. Du kannst mich aber gerne zu einem anderen Thema fragen.`, "Sage z.B. Hotel Dana Beach oder Das Adlon Berlin");
-        });
-      }).error( (err) => {
-          this.attributes[HOTEL_KEY] = null;
-          this.emit(':ask', `Zu ${hotel} weiß ich leider nichts. Du kannst mich aber gerne zu einem anderen Hotel fragen.`, "Sage z.B. Hotel Dana Beach oder Das Adlon Berlin");
-      });
+                this.emit(':tellWithCard', speechAnswer, hotelObj.name, cardTextAnswer, imageObj);
+            }).error((err) => {
+                  this.attributes[PASSION_KEY] = null;
+                  this.emit(':ask', `Zu Passion ${passion} weiß ich leider nichts. Du kannst mich aber gerne zu einem anderen Thema fragen.`, "Sage z.B. Hotel Dana Beach oder Das Adlon Berlin");
+            });
+          }).error( (err) => {
+              this.attributes[HOTEL_KEY] = null;
+              this.emit(':ask', `Zu ${hotel} weiß ich leider nichts. Du kannst mich aber gerne zu einem anderen Hotel fragen.`, "Sage z.B. Hotel Dana Beach oder Das Adlon Berlin");
+          })
+        };
     },
     'GetHotel': function() {
         const hotel = this.event.request.intent.slots.HOTEL.value;
